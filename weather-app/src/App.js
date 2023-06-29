75aab08d9f2b7f06c9811c23408b239ae13d5372
Elyas <Elyas.shamal2@gmail.com>
2023-06-29 19:28:45 -0400

    display the tempreture

---
+++ weather-app/src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = () =>
+      fetch("https://goweather.herokuapp.com/weather/Boston").then((res) =>
+        res.json()
+      );
+
+    async function startFetching() {
+      const weatherData = await fetchWeather();
+
+      setWeather(weatherData);
+    }
+    startFetching();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather Clock with react</h1>
+      </header>
+
+      <section>
+        <h1>Weather</h1>
+        <p>The current temperature is {weather?.temperature} degrees</p>
+      </section>
+
+      <section>
+        <h2>Clock</h2>
+        <p>The Time is {}</p>
+      </section>
+
+      {/* <aside className="right_side">
+
+        <header>
+          <h2>Change City</h2>
+          <button>X</button>
+        </header>
+      </aside> */}
+    </div>
+  );
+}
+
+export default App;
