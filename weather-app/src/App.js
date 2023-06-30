d2e9611840310ca9a66df86d555ec705a22ac98d
Elyas <Elyas.shamal2@gmail.com>
2023-06-29 21:28:29 -0400

    finished

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Clock from "./Clock";
 
 function App() {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    dialog.showModal();
+    return () => dialog.close();
+  });
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("Boston");
 
   useEffect(() => {
     const fetchWeather = () =>
-      fetch("https://goweather.herokuapp.com/weather/Boston").then((res) =>
+      fetch(`https://goweather.herokuapp.com/weather/${city}`).then((res) =>
         res.json()
       );
 
@@ -17,7 +25,7 @@ function App() {
       setWeather(weatherData);
     }
     startFetching();
-  }, []);
+  }, [city]);
 
   return (
     <div className="App">
@@ -37,13 +45,24 @@ function App() {
         </p>
       </section>
 
-      {/* <aside className="right_side">
-
+      <dialog ref={dialogRef}>
         <header>
-          <h2>Change City</h2>
-          <button>X</button>
+          <h1>Change City</h1>
+          <button
+            className="button-btn"
+            onClick={() => dialogRef.current.close()}
+          >
+            X
+          </button>
         </header>
-      </aside> */}
+        <label>Change City </label>
+        <select onChange={(e) => setCity(e.target.value)} value={city}>
+          <option>Chigago</option>
+          <option>Boston</option>
+          <option>Miami</option>
+          <option>North carolina</option>
+        </select>
+      </dialog>
     </div>
   );
 }
